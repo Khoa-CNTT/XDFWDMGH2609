e6f74c3dca853ad782cd75e20d5498b348ba2302
tranvanlong2k3 <tranvanlong4072003@gmail.com>
2025-04-10 03:04:02 +0700

    Second commit

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 
 const Navbar = () => {
+
+    const [menu,setMenu] = useState("home");
+
   return (
     <div className='navbar'>
-      
+      <img src={assets.logo} alt="" className="logo" />
+      <ul className='navbar-menu'>
+        <li  onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Trang chủ</li>
+        <li  onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Thực đơn</li>
+        <li  onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Ứng dụng di động</li>
+        <li  onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Liên Hệ</li>
+      </ul>
+      <div className='navbar-right'>
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Đăng Nhập</button>
+      </div>
     </div>
   )
 }
